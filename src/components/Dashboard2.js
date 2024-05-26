52b0130d0c302e40f47ca63e9ae80dd07258137b
Xinyi Zhou <xinyigix@uw.edu>
2024-05-26 02:36:09 -0700

    page2 1st version

--- src/components/Dashboard2.js
+++ src/components/Dashboard2.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Dashboard2.css';
+import testimage1 from '../assets/images/farm1.png';
+import testimage2 from '../assets/images/farm2.png';
+import testimage3 from '../assets/images/farm3.png';
+import logo1 from '../assets/images/logo.jpg';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 function Dashboard2() {
+  const [startDate, setStartDate] = useState(new Date());
+
+  const handleDateChange = (date) => {
+    setStartDate(date);
+  };
+
   return (
     <div className="dashboard">
       <header>
         <div className="logo">
-          <img src="https://via.placeholder.com/150" alt="Logo" />
+        <img src={logo1} alt="Row 4" className="logo" />
           <h1>Sentinel</h1>
         </div>
         <div className="profile">
@@ -26,36 +38,50 @@ function Dashboard2() {
             <h2>Device 1 Picture Library</h2>
             <div className="date-selector">
               <button>&lt;</button>
-              <span>May 2024</span>
+              <DatePicker 
+                selected={startDate} 
+                onChange={handleDateChange} 
+                dateFormat="MMMM d, yyyy"
+                className="date-picker"
+              />
               <button>&gt;</button>
             </div>
           </div>
           <div className="pictures">
             <div className="picture">
-              <div className="picture-time">capture time:</div>
-              <div className="peaewevil-num">
-                peaewevil num:
-                <button>-</button>
-                <span>n</span>
-                <button>+</button>
+              <img src={testimage1} alt="Row 1" className="testimage" />
+              <div className="picture-info">
+                <div className="picture-time">Capture time:</div>
+                <div className="peaewevil-num">
+                  Peaweevil num:
+                  <button>-</button>
+                  <span>n</span>
+                  <button>+</button>
+                </div>
               </div>
             </div>
             <div className="picture">
-              <div className="picture-time">capture time:</div>
-              <div className="peaewevil-num">
-                peaewevil num:
-                <button>-</button>
-                <span>n</span>
-                <button>+</button>
+              <img src={testimage2} alt="Row 2" className="testimage" />
+              <div className="picture-info">
+                <div className="picture-time">Capture time:</div>
+                <div className="peaewevil-num">
+                  Peaweevil num:
+                  <button>-</button>
+                  <span>n</span>
+                  <button>+</button>
+                </div>
               </div>
             </div>
             <div className="picture">
-              <div className="picture-time">capture time:</div>
-              <div className="peaewevil-num">
-                peaewevil num:
-                <button>-</button>
-                <span>n</span>
-                <button>+</button>
+              <img src={testimage3} alt="Row 3" className="testimage" />
+              <div className="picture-info">
+                <div className="picture-time">Capture time:</div>
+                <div className="peaewevil-num">
+                  Peaweevil num:
+                  <button>-</button>
+                  <span>n</span>
+                  <button>+</button>
+                </div>
               </div>
             </div>
           </div>
@@ -65,8 +91,8 @@ function Dashboard2() {
           <input type="range" min="1" max="24" />
           <button>Save Changes</button>
           <button>Apply to All</button>
-          <button>Capture Now</button>
-          <button>Request Maintenance</button>
+          <button className="biggerbutton">Capture Now</button>
+          <button className="biggerbutton">Request Maintenance</button>
         </div>
       </main>
     </div>
